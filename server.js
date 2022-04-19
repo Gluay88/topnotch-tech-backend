e43131b9c6983af63667fc34bbfbda83c6a1aa80
gluay <gluay@gluays-MacBook-Air.local>
2022-04-19 08:08:59 -0700

    backend

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+var dbconnection = require("./db");
+var productsRoute = require("./routes/productsRoute");
+
+app.use("./api/products/", productsRoute);
+
+app.get("/", (req, res) => {
+  res.send("This is from backend!!!");
+});
+// const port = process.env.PORT || 5000
+// app.listen(port, () => `Server running on port ${port}`)
+const port = 8000;
+app.listen(port, () => console.log(`Node JS Server Started on ${port}`));
